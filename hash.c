4b238ccbecd5e712b1d7836be0c12c14ffce3455
ashRaymon24 <ashraymon24@gmail.com>
2024-07-04 21:00:41 +0100

    Update hash.c

--- hash.c
+++ hash.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+#include <ctype.h>
 
+//hardcodes the size of the hash table
 #define TABLE_SIZE 31
+//prototypes of functions
 void setup(void);
 void addHash(void);
 void printTable(void);
 void subHash(void);
 void searchHash(void);
 
+//initialises hash table and previous table which is used to flag where a deletion has occured
 string hash_table [TABLE_SIZE];
 int prev_table [TABLE_SIZE];
 
 int main (void)
 {
+    //assigns every blank space a value
     for (int i = 0; i < TABLE_SIZE; i++)
     {
         hash_table[i] = "__";
@@ -31,6 +36,7 @@ void setup(void)
     int choice;
     printf("Enter 1 to ADD a person, Enter 2 to REMOVE a person, Enter 3 to SEARCH a person, Enter 4 to EXIT: ");
     scanf("%i", &choice);
+    //if user enters an unknown number, they are prompted to try again
     switch (choice)
     {
         case 1:
@@ -48,14 +54,17 @@ void setup(void)
     }
 }
 
+/* hashing function, takes the ASCII value of the uppercase letters of the first and last name, 
+multiplies both numbers by 6 and adds them together before taking the modulus of the table size */
 int hasher (string first, string last)
 {
-    int temp1 = first[0] - 64;
-    int temp2 = last[0] - 64;
+    int temp1 = toupper(first[0]) - 64;
+    int temp2 = toupper(last[0]) - 64;
     int hashed = ((temp1 * 6) + (temp2 * 6)) % TABLE_SIZE;
     return hashed;
 }
 
+//prints the hash table
 void printTable()
 {
     for (int i = 0; i < TABLE_SIZE; i++)
@@ -65,8 +74,10 @@ void printTable()
     return;
 }
 
+//adds the name onto the table
 void addHash()
 {
+    //takes the first and last names as inputs and concatenates them to form the full name
     char first_name[50];
     char last_name[50];
     char full_name[100];
@@ -79,6 +90,7 @@ void addHash()
     full_name[length] = 32;
     strcat(full_name, last_name);
 
+    //adds the value onto the position given by the hash function
     string temp = NULL;
     int position = hasher(first_name, last_name);
     temp = hash_table[position];
@@ -93,6 +105,7 @@ void addHash()
     return;
 }
 
+//searches for the name using the searching algorinthm and deletes it
 void subHash(void)
 {
     char first_name[50];
@@ -111,7 +124,10 @@ void subHash(void)
     string temp = NULL;
     int position = hasher(first_name, last_name);
     temp = hash_table[position];
-    while ((strcmp(temp, full_name) != 0) && ((strcmp(temp, "__") != 0) || (prev_table[position] = 1)))
+    //checks if the temp variable is not blank and is not the name, while also checking if temp is a blank space that was deleted previously
+    while ((strcmp(temp, full_name) != 0) && 
+            ((strcmp(temp, "__") != 0) || 
+            (prev_table[position] = 1)))
     {
         position += 1;
         if (position < TABLE_SIZE)
@@ -128,7 +144,7 @@ void subHash(void)
     else
     {
         hash_table[position] = "__";
-        prev_table[position] = 1;
+        prev_table[position] = 1; //adds the flag to show that a value has been deleted at that index
         printTable();
         printf("%s has been deleted\n", full_name);
     }
@@ -155,6 +171,7 @@ void searchHash(void)
     string temp = NULL;
     int position = hasher(first_name, last_name);
     temp = hash_table[position];
+    //uses same logic as subHash()
     while ((strcmp(temp, full_name) != 0) &&
             ((strcmp(temp, "__") != 0) ||
             (prev_table[position] = 1)))
